6690e5eb57a58dc158869ae11927ce2abc883c6c
blar khalid <blarharki@gmail.com>
2018-09-05 17:04:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-function produceDrivingRange(){
+function produceDrivingRange(blockRange){
   return function(startingBlock, endingBlock){
-     let start = parseInt(startingBlock);
-  let end = parseInt(endingBlock);
-      let distanceToTravel = Math.abs(end - start);
+
+    let start = parseInt(startingBlock);
+    let end = parseInt(endingBlock);
+    let distanceToTravel = Math.abs(end - start);
     let difference = blockRange - distanceToTravel;
 
     if( difference > 0){
