270617640d77ad87ed3f267105b69344c610c732
blar khalid <blarharki@gmail.com>
2018-09-05 17:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ function produceDrivingRange(){
   return function(startingBlock, endingBlock){
      var start = parseInt(startingBlock);
     var end = parseInt(endingBlock);
+      var distanceToTravel = Math.abs(end - start);
+    var difference = blockRange - distanceToTravel;
+
+    if( difference > 0){
+      return `within range by ${difference}`
+    } else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
   }
 }
